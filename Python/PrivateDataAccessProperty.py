c5ce51c757a08a22a55dcec6552f7f0334679d63
franciscofernandezlopez <frank.fernandez.lopez5@gmail.com>
2026-02-11 17:11:20 +0100

    simple_script (#1) #1

--- Python/PrivateDataAccessProperty.py
+++ Python/PrivateDataAccessProperty.py
@@ -9,10 +9,19 @@ class User:
         self._email = email # Python convention: an underscore shows a private attribute.
         self.password = password
     
-    
-    def get_email(self): # convention for getter methods: prefix the name the attribute with "get"
-        print(f"Email accesed at {datetime.now()}.")
+    @property # The property decorator permits turns this method into a getter property, which we can access it using class.method
+    def email(self):
+        print('Email accessed.')
         return self._email
+    
+    @email.setter
+    def email(self, new_email):
+        if "@" in new_email:
+            self._email = new_email
+    
+    # def get_email(self): # convention for getter methods: prefix the name the attribute with "get"
+    #     print(f"Email accesed at {datetime.now()}.")
+    #     return self._email
     
     def set_email(self, new_email):
         if "@" in new_email:
@@ -21,10 +30,11 @@ class User:
 # Protected values should not be updated outside of the class
         
 user1 = User("dantheman", email = "Dan@gmail.com", password = "123")
-print(user1.get_email())
+print(user1.email)
 
-# Why user setter and getterrs ?
+# Why use properties?
 
-# It gives us full control with what happens when an attribute is modified
+# It gives us full control with what happens when an attribute is modified or accessed
+# It is less verbose than a get or set methods
 
 user1.set_email("123") # Does not change email since it is not "valid"
