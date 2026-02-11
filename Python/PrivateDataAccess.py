7a17c9bb24c274a19eb3b492b068c5dff8092a5a
franciscofernandezlopez <frank.fernandez.lopez5@gmail.com>
2026-02-11 10:43:16 +0100

    Adjustments and comments

--- Python/PrivateDataAccess.py
+++ Python/PrivateDataAccess.py
@@ -6,14 +6,17 @@ class User:
         self.username = username
         self._email = email # Python convention: an underscore shows a private attribute.
         self.password = password
-        
-    # def get_email():
-    #     return self._email
-
-    def clean_email(self):
-        self._email.lower()
-        return
     
-user1 = User("dantheman", email = "dan@gmail.com", password = "123")
+    def get_email(self): # convention for getter methods: prefix the name the attribute with "get"
+        return self._email
+    
+    def set_email(self, new_email):
+        self._email = new_email
+        
+# Protected values should not be updated outside of the class
+        
+user1 = User("dantheman", email = "Dan@gmail.com", password = "123")
+print(user1.get_email())
 
-print(user1._email) # This DOESn't throw an error. The protected attribute is accesible outside of the class.
+user1 = User("dantheman", email = "Dan@gmail.com", password = "123")
+print(user1.get_email())
